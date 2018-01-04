8f4fb2b66c3a9a610fba8ae9537724bba8c27a25
soorajv.c <soorajv.c@ivedix.com>
2018-01-04 14:25:42 +0530

    Topshef Reference added

--- SchedulerService/Program.cs
+++ SchedulerService/Program.cs
@@ -1,23 +1,25 @@
-﻿namespace SchedulerService
+﻿using Topshelf;
+
+namespace SchedulerService
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //    HostFactory.Run(serviceConfig =>
-            //    {
-            //        serviceConfig.Service<ScheduleService>(serviceInstance =>
-            //        {
-            //            serviceInstance.ConstructUsing(() => new ScheduleService());
-            //            serviceInstance.WhenStarted(execute => execute.Start());
-            //            serviceInstance.WhenStopped(execute => execute.Stop());
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.Service<ScheduleService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(() => new ScheduleService());
+                    serviceInstance.WhenStarted(execute => execute.Start());
+                    serviceInstance.WhenStopped(execute => execute.Stop());
 
-            //        });
-            //        serviceConfig.SetServiceName("WiXScheduler");
-            //        serviceConfig.SetDisplayName("WiX Scheduler");
-            //        serviceConfig.SetDescription("WiX Scheduler");
-            //        serviceConfig.StartAutomatically();
-            //    });
+                });
+                serviceConfig.SetServiceName("WiXSchedulerService");
+                serviceConfig.SetDisplayName("WiX Scheduler Service");
+                serviceConfig.SetDescription("WiX Scheduler Service");
+                serviceConfig.StartAutomatically();
+            });
         }
     }
 }
