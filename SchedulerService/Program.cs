7a115df2356c999302b5a0aed66ee2dbd2f4751f
soorajv.c <soorajv.c@ivedix.com>
2018-01-04 14:18:40 +0530

    Add project files.

---
+++ SchedulerService/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SchedulerService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    HostFactory.Run(serviceConfig =>
+            //    {
+            //        serviceConfig.Service<ScheduleService>(serviceInstance =>
+            //        {
+            //            serviceInstance.ConstructUsing(() => new ScheduleService());
+            //            serviceInstance.WhenStarted(execute => execute.Start());
+            //            serviceInstance.WhenStopped(execute => execute.Stop());
+
+            //        });
+            //        serviceConfig.SetServiceName("WiXScheduler");
+            //        serviceConfig.SetDisplayName("WiX Scheduler");
+            //        serviceConfig.SetDescription("WiX Scheduler");
+            //        serviceConfig.StartAutomatically();
+            //    });
+        }
+    }
+}
---
+++ SchedulerService/ScheduleService.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+
+namespace SchedulerService
+{
+    class ScheduleService
+    {
+        System.Timers.Timer timeDelay;
+        int count;
+        public ScheduleService()
+        {
+            timeDelay = new System.Timers.Timer();
+            timeDelay.Elapsed += new System.Timers.ElapsedEventHandler(WorkProcess);
+        }
+        public bool Start()
+        {
+            LogService("Service is Started");
+            timeDelay.Enabled = true;
+
+            return true;
+        }
+        public void WorkProcess(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            string process = "Timer Tick " + count;
+            LogService(process);
+            count++;
+        }
+
+        public bool Stop()
+        {
+            LogService("Service Stoped");
+            timeDelay.Enabled = false;
+
+            return true;
+        }
+        private void LogService(string content)
+        {
+            var fs = new FileStream(@"c:\temp\a\WiXServiceLog.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            using (var sw = new StreamWriter(fs))
+            {
+                sw.BaseStream.Seek(0, SeekOrigin.End);
+                sw.WriteLine(content);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+
+
+    }
+}
